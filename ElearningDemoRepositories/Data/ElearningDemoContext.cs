4d5714c80cf0d9e1278a343e3ead714c6a58d53f
joe.chien <pdono12345@gmail.com>
2022-07-05 12:08:34 +0800

    Remove unnecercery usings.

--- ElearningDemoRepositories/Data/ElearningDemoContext.cs
+++ ElearningDemoRepositories/Data/ElearningDemoContext.cs
@@ -1,29 +1,25 @@
-﻿using ElearningDemoRepositories.Entities;
-using Microsoft.EntityFrameworkCore;
+﻿namespace ElearningDemoRepositories.Data;
 
-namespace ElearningDemoRepositories.Data
+public class ElearningDemoContext : DbContext
 {
-    public class ElearningDemoContext : DbContext
+    public ElearningDemoContext(DbContextOptions options) : base(options)
     {
-        public ElearningDemoContext(DbContextOptions options) : base(options)
-        {
-        }
+    }
 
-        public DbSet<Classroom> Classrooms { get; set; }
-        public DbSet<Manager> Managers { get; set; }
-        public DbSet<Member> Members { get; set; }
-        public DbSet<Quiz> Quizzes { get; set; }
-        public DbSet<Student> Students { get; set; }
-        public DbSet<Teacher> Teachers { get; set; }
+    public DbSet<Classroom> Classrooms { get; set; } = null!;
+    public DbSet<Manager> Managers { get; set; } = null!;
+    public DbSet<Member> Members { get; set; } = null!;
+    public DbSet<Quiz> Quizzes { get; set; } = null!;
+    public DbSet<Student> Students { get; set; } = null!;
+    public DbSet<Teacher> Teachers { get; set; } = null!;
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Classroom>().ToTable(nameof(Classroom));
-            modelBuilder.Entity<Manager>().ToTable(nameof(Manager));
-            modelBuilder.Entity<Member>().ToTable(nameof(Member));
-            modelBuilder.Entity<Quiz>().ToTable(nameof(Quiz));
-            modelBuilder.Entity<Student>().ToTable(nameof(Student));
-            modelBuilder.Entity<Teacher>().ToTable(nameof(Teacher));
-        }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Classroom>().ToTable(nameof(Classroom));
+        modelBuilder.Entity<Manager>().ToTable(nameof(Manager));
+        modelBuilder.Entity<Member>().ToTable(nameof(Member));
+        modelBuilder.Entity<Quiz>().ToTable(nameof(Quiz));
+        modelBuilder.Entity<Student>().ToTable(nameof(Student));
+        modelBuilder.Entity<Teacher>().ToTable(nameof(Teacher));
     }
 }
