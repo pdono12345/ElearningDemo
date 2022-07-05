f6ea53fc874e19996ffb65c2b2f01d013fa3111b
joe.chien <pdono12345@gmail.com>
2022-07-05 11:36:01 +0800

    Create BaseController

---
+++ ElearningDemo/Controllers/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElearningDemo.Controllers;
+[Authorize(AuthenticationSchemes = "User_Schema")]
+public class BaseController : Controller
+{
+    protected readonly ILogger _logger;
+    protected readonly IConfiguration _config;
+    protected readonly IMapper _mapper;
+
+    public BaseController(ILogger logger, IConfiguration config, IMapper mapper)
+    {
+        _logger = logger;
+        _config = config;
+        _mapper = mapper;
+    }
+
+}
+
