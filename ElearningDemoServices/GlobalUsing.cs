9722866ed55dcb8d4fca821f109091ff42d1bf19
joe.chien <pdono12345@gmail.com>
2022-07-05 11:56:29 +0800

    Create GlobalUsings of ServiceLayer

---
+++ ElearningDemoServices/GlobalUsing.cs
@@ -0,0 +1,7 @@
+﻿global using AutoMapper;
+global using ElearningDemoRepositories.IRepositories;
+global using ElearningDemoRepositories.Entities;
+global using ElearningDemoServices.IServices;
+global using ElearningDemoServices.DTOs;
+
+
