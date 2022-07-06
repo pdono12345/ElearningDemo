644ebb18a29d3c5cdf755b7fd9205ac4b862b25d
joe.chien <pdono12345@gmail.com>
2022-07-06 10:51:10 +0800

    Add DefaultAdminViewDataAttribute

--- ElearningDemo/Areas/Admin/Controllers/HomeController.cs
+++ ElearningDemo/Areas/Admin/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ElearningDemo.Areas.Admin.Filters;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ElearningDemo.Areas.Admin.Controllers;
+[TypeFilter(typeof(DefaultAdminViewDataAttribute))]
 public class HomeController : BaseController
 {
     public HomeController(ILogger<HomeController> logger, IConfiguration config, IMapper mapper) : base(logger, config, mapper)
